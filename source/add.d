7c2d33268d607f5ab36a7fbece0c919f9b4047a2
magley <60552482+magley@users.noreply.github.com>
2025-04-25 12:20:43 +0200

    fix: Don't allow 2+ libs with the same `name`

--- source/add.d
+++ source/add.d
@@ -2,10 +2,19 @@ import project;
 import input;
 import coalfile;
 import cli;
+import core.stdc.stdlib;
+import std.algorithm;
+import std.stdio;
 
 void do_add(ref Command_add cmd)
 {
 	Project p = load();
+
+	if (p.libs.any!(l => l.name == cmd.name.get))
+	{
+		writefln("A library by the name of %s is already defined", cmd.name.get);
+		exit(1);
+	}
 
 	Library lib = Library();
 	lib.name = cmd.name.get;
